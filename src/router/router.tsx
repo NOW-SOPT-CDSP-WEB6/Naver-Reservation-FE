ebcd1b29b41018c0cef9e5aeb63ff89d165b5ad4
wuzoo <wndyd9911@gmail.com>
2024-05-14 20:26:56 +0900

    #5 :sparkles: feat: 4개 view 라우트 설계

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,1 +1,37 @@
-/** router 정의 */
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+import DonePage from '@/pages/donePage/DonePage';
+import HomePage from '@/pages/homePage/HomePage';
+import KeywordPage from '@/pages/keywordPage/KeywordPage';
+import ReviewPage from '@/pages/reviewPage/ReviewPage';
+
+import App from '@/App';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: 'keyword',
+        element: <KeywordPage />,
+      },
+      {
+        path: 'review',
+        element: <ReviewPage />,
+      },
+      {
+        path: 'done',
+        element: <DonePage />,
+      },
+    ],
+  },
+]);
+
+export const AppRouter = () => {
+  return <RouterProvider router={router} />;
+};
