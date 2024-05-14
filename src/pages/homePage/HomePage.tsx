96633699bd95c50ac753f2114181779297580ab0
wuzoo <wndyd9911@gmail.com>
2024-05-14 21:04:52 +0900

    #5 :art: chore: homePage 개발 중인 컴포넌트 삭제 후 초기 파일로

--- src/pages/homePage/HomePage.tsx
+++ src/pages/homePage/HomePage.tsx
@@ -1,19 +1,7 @@
-import HomeBtn from '@/components/home/homeBtn/HomeBtn';
-
-interface HomePageProps {}
-
-const HomePage = ({}: HomePageProps) => {
+const HomePage = () => {
   return (
     <>
       <h1>Home Page</h1>
-      <HomeBtn category="all" />
-      <HomeBtn category="beauty" />
-      <HomeBtn category="health" />
-      <HomeBtn category="life" />
-      <HomeBtn category="living" />
-      <HomeBtn category="restaurant" />
-      <HomeBtn category="sport" />
-      <HomeBtn category="ticket" />
     </>
   );
 };
