0493f9a647881131cefda71c5e60200b134ed0c9
Lee jin <83453646+j-nary@users.noreply.github.com>
2024-05-16 05:40:48 +0900

    #19 :sparkles: feat: drag&drop view 구현

--- src/pages/reviewPage/DragDrop/DragDrop.style.ts
+++ src/pages/reviewPage/DragDrop/DragDrop.style.ts
@@ -2,5 +2,20 @@ import { Theme } from "@/styles/theme";
 import { css } from "@emotion/react";
 
 export const Wrapper = css({
-    backgroundColor: "red",
+    display: "flex",
+    flexDirection: "column",
+
+    justifyContent: "center",
+    alignItems: "center",
+    
+    width: "100%",
+    height: "100%",
+})
+
+export const text = css({
+    marginBottom: "7px"
+})
+export const plusBtn = css({
+    width: "49px",
+    height: "49px"
 })
--- src/pages/reviewPage/DragDrop/DragDrop.tsx
+++ src/pages/reviewPage/DragDrop/DragDrop.tsx
@@ -1,4 +1,6 @@
 import ContentBox from "@/components/@common/ContentBox/ContentBox";
+import { Wrapper, plusBtn, text } from "@/pages/reviewPage/DragDrop/DragDrop.style";
+import PlusBtn from "@/assets/svgs/review/review_btn_plus.svg?react"
 
 const DragDrop = () => {
   return (
@@ -7,9 +9,11 @@ const DragDrop = () => {
       styles={{
         height: '180px',
         marginTop: '20px',
-        backgroundColor: 'red',
       }}>
-      안녕하세요
+      <section css={Wrapper}>
+        <span css={text}>사진을 추가해 주세요</span>
+        <PlusBtn css={plusBtn}/>
+      </section>
     </ContentBox>
   );
 };
