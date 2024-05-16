100ce946048ec42b10982ea9d6ab32f553eb30ff
wuzoo <wndyd9911@gmail.com>
2024-05-16 15:45:42 +0900

    #5 :art: chore: 불필요 import 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { Outlet } from 'react-router-dom';
 
-import ContentBox from '@/components/@common/ContentBox/ContentBox';
 import Header from '@/components/layout/header/Header';
 import MobileLayout from '@/components/layout/mobileLayout/MobileLayout';
 
