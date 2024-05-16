55d7963225a3a5d993a0a1245e123f6cdc77fc4f
wuzoo <wndyd9911@gmail.com>
2024-05-16 16:02:46 +0900

    #7 :sparkles: feat: Category 관련 상수 데이터 선언

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,1 +1,21 @@
-/** 상수 데이터 선언 */
+export const CATEGORY = {
+  all: '전체',
+  beauty: '뷰티',
+  life: '생활/클래스',
+  restaurant: '식당/카페',
+  living: '숙박',
+  sport: '스포츠/레저',
+  ticket: '공연/전시',
+  health: '병의원',
+};
+
+export const CATEGORY_MENU = [
+  CATEGORY.all,
+  CATEGORY.beauty,
+  CATEGORY.life,
+  CATEGORY.restaurant,
+  CATEGORY.living,
+  CATEGORY.sport,
+  CATEGORY.ticket,
+  CATEGORY.health,
+];
