77dbe8e39d64f3d8edc967c41afbd13098c2317b
wuzoo <wndyd9911@gmail.com>
2024-05-16 17:50:06 +0900

    #7 :sparkles: feat: API 명세 수정에 따른 프로퍼티 수정 + day 문자열 출력 위한 DAY_IN_WEEKS 정의

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,21 +1,23 @@
 export const CATEGORY = {
-  all: '전체',
-  beauty: '뷰티',
-  life: '생활/클래스',
-  restaurant: '식당/카페',
-  living: '숙박',
-  sport: '스포츠/레저',
-  ticket: '공연/전시',
-  health: '병의원',
+  전체: 'all',
+  뷰티: 'beauty',
+  '생활/클래스': 'life_class',
+  '식당/카페': 'restaurant_cafe',
+  숙박: 'accommodation',
+  '스포츠/레저': 'sports_leisure',
+  '공연/전시': 'performance_exhibition',
+  병의원: 'hospital',
 };
 
-export const CATEGORY_MENU = [
-  CATEGORY.all,
-  CATEGORY.beauty,
-  CATEGORY.life,
-  CATEGORY.restaurant,
-  CATEGORY.living,
-  CATEGORY.sport,
-  CATEGORY.ticket,
-  CATEGORY.health,
-];
+export const DAY_IN_WEEKS = ['일', '월', '화', '수', '목', '금', '토', '일'];
+
+// export const CATEGORY_MENU = [
+//   CATEGORY.all,
+//   CATEGORY.beauty,
+//   CATEGORY.life,
+//   CATEGORY.restaurant,
+//   CATEGORY.living,
+//   CATEGORY.sport,
+//   CATEGORY.ticket,
+//   CATEGORY.health,
+// ];
