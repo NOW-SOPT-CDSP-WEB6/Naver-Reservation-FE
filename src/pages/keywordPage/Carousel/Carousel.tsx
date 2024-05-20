25f7d64316b332e6334b5df4bdbb15230de055d5
Lee jin <83453646+j-nary@users.noreply.github.com>
2024-05-21 02:47:16 +0900

    #34 :sparkles: feat: Carousel 구현

--- src/pages/keywordPage/Carousel/Carousel.tsx
+++ src/pages/keywordPage/Carousel/Carousel.tsx
@@ -1,17 +1,35 @@
 import ContentBox from "@/components/@common/ContentBox/ContentBox";
+import * as s from "./Carousel.style"
+import Text from "@/components/@common/Text/Text";
+import { BADGE_CATEGORY } from "@/constants/badge";
+import CarouselCard from "@/pages/keywordPage/CarouselCard/CarouselCard";
 
-interface CarouselProps {}
+interface CarouselProps { }
 
-const Carousel = ({}: CarouselProps) => {
+
+const Carousel = ({ }: CarouselProps) => {
+  const category = "뷰티";
+
   return (
     <ContentBox
       variant='round'
       styles={{
-        height: '434px',
         marginTop: '12px',
+        padding: '26px 0 42px 20px',
       }}
     >
-      
+      <header css={s.header}>
+        <div css={s.title}>어떤 점이 좋았나요?</div>
+        <div css={s.tag}>필수</div>
+      </header>
+      <Text size="medium" css={s.detail}>이 가게에 어울리는 키워드를 골라주세요.(1개~5개)</Text>
+      <section css={s.carouselWrapper}>
+        {
+          BADGE_CATEGORY[category].map((badge) => {
+            return (<CarouselCard badgeTitle={badge} />)
+          })
+        }
+      </section>
     </ContentBox>
   );
 };
