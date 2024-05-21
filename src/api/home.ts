d1bd69c2fae7faa11468ce24dd5a58a43b0f9e70
wuzoo <wndyd9911@gmail.com>
2024-05-22 00:43:29 +0900

    #36 :recycle: refactor: data의 data 필드를 반환하도록 구현

--- src/api/home.ts
+++ src/api/home.ts
@@ -1,11 +1,7 @@
 import { apiRequest } from '@/api/axiosinstance';
 
 export const getAllReservation = async () => {
-  const { data } = await apiRequest.get('api/v1/reservations/all', {
-    headers: {
-      memberId: 1,
-    },
-  });
+  const { data } = await apiRequest.get('api/v1/reservations/all');
 
   return data.data;
 };
