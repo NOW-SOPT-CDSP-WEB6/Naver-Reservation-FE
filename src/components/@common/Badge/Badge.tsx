cc6ca47fffba34b4acd451dbb3e62b25b5f1f2d7
wuzoo <wndyd9911@gmail.com>
2024-05-23 02:40:25 +0900

    #45 :art: chore: import 수정

--- src/components/@common/Badge/Badge.tsx
+++ src/components/@common/Badge/Badge.tsx
@@ -1,17 +1,14 @@
-import { badgeStyle, customStyle } from "@/components/@common/Badge/Badge.style";
-import { ReactNode } from "react";
+import { ReactNode } from 'react';
+
+import { badgeStyle, customStyle } from '@/components/@common/Badge/Badge.style';
 
 interface BadgeProps {
-    size: "default" | "small",
-    children?: ReactNode,
+  size: 'default' | 'small';
+  children?: ReactNode;
 }
 
-const Badge = ({size, children}: BadgeProps) => {
-  return (
-    <div css={[badgeStyle, customStyle[size]]}>
-        {children}
-    </div>
-  );
+const Badge = ({ size, children }: BadgeProps) => {
+  return <div css={[badgeStyle, customStyle[size]]}>{children}</div>;
 };
 
 export default Badge;
