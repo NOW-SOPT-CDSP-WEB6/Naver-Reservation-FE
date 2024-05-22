d11b597b5c59ce5c78854748650967c00eaa752a
wuzoo <wndyd9911@gmail.com>
2024-05-22 23:49:46 +0900

    #45 :sparkles: feat: home page api 생성

--- src/api/home.ts
+++ src/api/home.ts
@@ -1,7 +1,23 @@
 import { apiRequest } from '@/api/axiosinstance';
 
+import { CATEGORY } from '@/constants';
+
 export const getAllReservation = async () => {
   const { data } = await apiRequest.get('api/v1/reservations/all');
 
   return data.data;
+};
+
+export const getMyReservationInfo = async (category: string) => {
+  const categoryString = category === '전체' ? '' : '/' + CATEGORY[category];
+
+  const { data } = await apiRequest.get(`api/v1/reservations${categoryString}`);
+
+  return data.data;
+};
+
+export const postReservationMark = async (reservationId: number) => {
+  const { data } = await apiRequest.post(`api/v1/reservations/star/${reservationId}`);
+
+  return data;
 };
