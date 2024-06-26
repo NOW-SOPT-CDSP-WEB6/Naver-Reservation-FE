20d51fc53253ca6f6104ed95e74fafd3d2e12da0
wuzoo <wndyd9911@gmail.com>
2024-06-26 16:27:24 +0900

    test: lighthouse 측정을 위해 lazy 제외하여 커밋

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,16 +1,16 @@
-import { Suspense, lazy } from 'react';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
-// import DonePage from '@/pages/donePage/DonePage';
-// import HomePage from '@/pages/homePage/HomePage';
-// import KeywordPage from '@/pages/keywordPage/KeywordPage';
-// import ReviewPage from '@/pages/reviewPage/ReviewPage';
+import DonePage from '@/pages/donePage/DonePage';
+import HomePage from '@/pages/homePage/HomePage';
+import KeywordPage from '@/pages/keywordPage/KeywordPage';
+import ReviewPage from '@/pages/reviewPage/ReviewPage';
+
 import App from '@/App';
 
-const DonePage = lazy(() => import('@/pages/donePage/DonePage'));
-const HomePage = lazy(() => import('@/pages/homePage/HomePage'));
-const KeywordPage = lazy(() => import('@/pages/keywordPage/KeywordPage'));
-const ReviewPage = lazy(() => import('@/pages/reviewPage/ReviewPage'));
+// const DonePage = lazy(() => import('@/pages/donePage/DonePage'));
+// const HomePage = lazy(() => import('@/pages/homePage/HomePage'));
+// const KeywordPage = lazy(() => import('@/pages/keywordPage/KeywordPage'));
+// const ReviewPage = lazy(() => import('@/pages/reviewPage/ReviewPage'));
 
 const router = createBrowserRouter([
   {
@@ -19,35 +19,19 @@ const router = createBrowserRouter([
     children: [
       {
         index: true,
-        element: (
-          <Suspense>
-            <HomePage />
-          </Suspense>
-        ),
+        element: <HomePage />,
       },
       {
         path: 'keyword/:reservationId',
-        element: (
-          <Suspense>
-            <KeywordPage />
-          </Suspense>
-        ),
+        element: <KeywordPage />,
       },
       {
         path: 'review/:reservationId',
-        element: (
-          <Suspense>
-            <ReviewPage />
-          </Suspense>
-        ),
+        element: <ReviewPage />,
       },
       {
         path: 'done/:reservationId',
-        element: (
-          <Suspense>
-            <DonePage />
-          </Suspense>
-        ),
+        element: <DonePage />,
       },
     ],
   },
