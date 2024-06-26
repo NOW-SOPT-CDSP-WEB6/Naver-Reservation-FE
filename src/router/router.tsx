f1e4c766f0ed700250b466fe8a84ab560bc740c4
wuzoo <wndyd9911@gmail.com>
2024-06-26 16:00:50 +0900

    feat: code split 적용

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,11 +1,16 @@
+import { Suspense, lazy } from 'react';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
-import DonePage from '@/pages/donePage/DonePage';
-import HomePage from '@/pages/homePage/HomePage';
-import KeywordPage from '@/pages/keywordPage/KeywordPage';
-import ReviewPage from '@/pages/reviewPage/ReviewPage';
-
+// import DonePage from '@/pages/donePage/DonePage';
+// import HomePage from '@/pages/homePage/HomePage';
+// import KeywordPage from '@/pages/keywordPage/KeywordPage';
+// import ReviewPage from '@/pages/reviewPage/ReviewPage';
 import App from '@/App';
+
+const DonePage = lazy(() => import('@/pages/donePage/DonePage'));
+const HomePage = lazy(() => import('@/pages/homePage/HomePage'));
+const KeywordPage = lazy(() => import('@/pages/keywordPage/KeywordPage'));
+const ReviewPage = lazy(() => import('@/pages/reviewPage/ReviewPage'));
 
 const router = createBrowserRouter([
   {
@@ -14,19 +19,35 @@ const router = createBrowserRouter([
     children: [
       {
         index: true,
-        element: <HomePage />,
+        element: (
+          <Suspense>
+            <HomePage />
+          </Suspense>
+        ),
       },
       {
         path: 'keyword/:reservationId',
-        element: <KeywordPage />,
+        element: (
+          <Suspense>
+            <KeywordPage />
+          </Suspense>
+        ),
       },
       {
         path: 'review/:reservationId',
-        element: <ReviewPage />,
+        element: (
+          <Suspense>
+            <ReviewPage />
+          </Suspense>
+        ),
       },
       {
         path: 'done/:reservationId',
-        element: <DonePage />,
+        element: (
+          <Suspense>
+            <DonePage />
+          </Suspense>
+        ),
       },
     ],
   },
